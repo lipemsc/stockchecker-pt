7afb6e5aceb57e66c516f110baed32e1e5385870
lipemsc <lipe.msc@gmai.com>
2020-12-02 04:16:22 +0000

    Added loop to constantly monitor the stock. Now once the program starts, it will check each 5 seconds for stock. Thats was accomplished using recursion. I tried without it but it would eventually fail, sending to many requests to the server, potentially triggering any DDoS protection from it.

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 const https = require('https');
 const HTMLParser = require('node-html-parser');
-const pageToCheck = "https://www.pcdiga.com/smartphone-xiaomi-redmi-note-9-6-53-3gb-64gb-dual-sim-preto";
+const pageToCheck = "https://www.pcdiga.com/alisador-de-cabelo-rowenta-for-elite-optiliss";
 const parseOptions = {blockTextElements:{script:true,noscript:true,style:true,pre:true}};
 const pageURL = new URL(pageToCheck);
-console.log(pageURL.hostname);
 
+function getPage() {
+  var htmlPage = "";
+  var request = https.get(pageToCheck, (response) => {
+    response.on('data', (data) => {
+      htmlPage += data;
+    });
 
+    response.on('end', () => {
+      var currentTime = new Date();
+      currentTime.setTime(Date.now());
+      console.log(currentTime.getUTCFullYear() + "-" + (("0" + (currentTime.getUTCMonth()+1)).slice(-2)) + "-" + ("0" + currentTime.getUTCDate()).slice(-2) + " " + ("0" + currentTime.getUTCHours()).slice(-2) + ":" + ("0" + currentTime.getUTCMinutes()).slice(-2) + ":" + ("0" + currentTime.getUTCSeconds()).slice(-2) + " - " + processPage(htmlPage));
+      setTimeout(getPage, 5000);
+    });
 
-var request = https.get(pageToCheck, (response) => {
-  var htmlpage = "";
-  
-  response.on('data', (data) => {
-    htmlpage += data;
   });
+}
 
-  response.on('end', () => {
-    
-    switch (pageURL.hostname) {
-      case "pcdiga.com":
-      case "www.pcdiga.com":
-        if (htmlpage.slice(htmlpage.search("is_in_stock") + 14, htmlpage.search("is_in_stock") + 15) == "1")
-          console.log("Em stock");
-        else
-          console.log("Esgotado");
-        break;
-      case "globaldata.pt": 
-      case "www.globaldata.pt": 
-        var page = HTMLParser.parse(htmlpage,parseOptions);
-        page.querySelectorAll(".stock-shops")[0].removeWhitespace();
-        console.log(page.querySelectorAll(".stock-shops")[0].childNodes[0].innerHTML);
-        break;
-    
-    
-    
-    }
-    
-  });
+function processPage (pageData) {    
+  switch (pageURL.hostname) {
+    case "pcdiga.com":
+    case "www.pcdiga.com":
+      if (pageData.slice(pageData.search("is_in_stock") + 14, pageData.search("is_in_stock") + 15) == "1")
+        return "Em stock";
+      else
+        return "Esgotado";
+      break;
+    case "globaldata.pt": 
+    case "www.globaldata.pt": 
+      var parsedPage = HTMLParser.parse(pageData,parseOptions);
+      parsedPage.querySelectorAll(".stock-shops")[0].removeWhitespace();
+      return parsedPage.querySelectorAll(".stock-shops")[0].childNodes[0].innerHTML;
+      break;
+    default:
+      return "Site não suportado";
+  }
+};
 
-
-});
-
+getPage();
