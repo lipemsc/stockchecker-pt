083dd1ef74f4d727a699f2dfbc3a1f781d2b518f
lipemsc <lipe.msc@gmail.com>
2020-12-04 18:57:15 +0000

    Added help and ability to set URL in args. URL is now set in the process arguments. Added a help option by using the -h argument.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const https = require('https');
 const HTMLParser = require('node-html-parser');
-const pageToCheck = "https://www.pcdiga.com/alisador-de-cabelo-rowenta-for-elite-optiliss";
 const parseOptions = {blockTextElements:{script:true,noscript:true,style:true,pre:true}};
-const pageURL = new URL(pageToCheck);
 
-function getPage() {
+function printHelp() {
+  console.log("Utilização: node index.js \[\<URL\>\] \[-h|--help\] \nOpções\:\n  --help | -h       Mostra esta ajuda e sai.");
+}
+
+function pcdiga(pageData) {
+  if (pageData.slice(pageData.search("is_in_stock") + 14, pageData.search("is_in_stock") + 15) == "1")
+    return "Em stock";
+  else if(pageData.slice(pageData.search("is_in_stock") + 14, pageData.search("is_in_stock") + 15) == "0")
+    return "Esgotado";
+  else
+    return "Erro"
+}
+
+function globaldata(pageData) {
+  var parsedPage = HTMLParser.parse(pageData,parseOptions);
+  parsedPage.querySelectorAll(".stock-shops")[0].removeWhitespace();
+  return parsedPage.querySelectorAll(".stock-shops")[0].childNodes[0].innerHTML;
+}
+
+function getPage(url) {
   var htmlPage = "";
-  var request = https.get(pageToCheck, (response) => {
+  var request = https.get(pageURL.toString(), (response) => {
     response.on('data', (data) => {
       htmlPage += data;
     });
 
     response.on('end', () => {
+      var result;
       var currentTime = new Date();
       currentTime.setTime(Date.now());
       console.log(currentTime.getUTCFullYear() + "-" + (("0" + (currentTime.getUTCMonth()+1)).slice(-2)) + "-" + ("0" + currentTime.getUTCDate()).slice(-2) + " " + ("0" + currentTime.getUTCHours()).slice(-2) + ":" + ("0" + currentTime.getUTCMinutes()).slice(-2) + ":" + ("0" + currentTime.getUTCSeconds()).slice(-2) + " - " + processPage(htmlPage));
@@ -25,20 +43,40 @@ function processPage (pageData) {
   switch (pageURL.hostname) {
     case "pcdiga.com":
     case "www.pcdiga.com":
-      if (pageData.slice(pageData.search("is_in_stock") + 14, pageData.search("is_in_stock") + 15) == "1")
-        return "Em stock";
-      else
-        return "Esgotado";
+      return pcdiga(pageData);
       break;
     case "globaldata.pt": 
     case "www.globaldata.pt": 
-      var parsedPage = HTMLParser.parse(pageData,parseOptions);
-      parsedPage.querySelectorAll(".stock-shops")[0].removeWhitespace();
-      return parsedPage.querySelectorAll(".stock-shops")[0].childNodes[0].innerHTML;
+      return globaldata(pageData);
       break;
     default:
-      return "Site não suportado";
+      return false;
   }
 };
 
-getPage();
+if (typeof process.argv[2] !== 'undefined') {
+  if(process.argv[2] == "--help" || process.argv[2] == "-h")
+    printHelp();
+  else {
+    try {
+      var pageURL = new URL(process.argv[2]);
+      switch (pageURL.hostname) {
+        case "pcdiga.com":
+        case "www.pcdiga.com":
+        case "globaldata.pt": 
+        case "www.globaldata.pt":
+          break;
+        default:
+          console.log("Erro: Website não suportado");
+          return false;
+      }
+      getPage(pageURL);
+    }
+    catch {
+      console.log("Erro: url incorreto.\nUse --help para obter ajuda.");
+    }
+  }
+}
+else
+  console.log("Erro: url inexistente.\nUse --help para obter ajuda.");
+
